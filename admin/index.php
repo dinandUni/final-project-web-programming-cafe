85e8e278e8db1a1fee202cb7507af111bb37fec8
ChaserHades <ifofkgt@gmail.com>
2024-12-03 05:19:20 +0700

    Home, Admin
    PHP, CSS

---
+++ admin/index.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>
+            Food Order Website - Home Page
+        </title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+    <body>
+        <!-- Menu -->
+        <div class="menu">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Admin</a></li>
+                    <li><a href="#">Category</a></li>
+                    <li><a href="#">Food</a></li>
+                    <li><a href="#">Order</a></li>
+                </ul>
+            </div>
+        </div>
+        <!-- Menu End-->
+        
+        <!-- Main -->
+        <div class="menu">
+            <div class="wrapper">
+                AKU SETRESS
+            </div>
+        </div>
+        <!-- Main End -->
+
+        <!-- Footer -->
+        <div class="menu">
+            <div class="wrapper">
+                <p class="text-center">2024 Project Web Terbaekk!!<a href="#">?</a></p>
+            </div>
+        </div>
+        <!-- Footer End -->
+    </body>
+</html>
