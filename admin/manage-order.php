0f11acc458c6c827a608a4722d79437dcbbfae07
dinandUni <672023007@student.uksw.edu>
2024-12-04 06:13:13 +0700

    sambung punya stef dan velikx

--- admin/manage-order.php
+++ admin/manage-order.php
@@ -4,50 +4,94 @@
         <div class="wrapper">
             <h1>Manage Order</h1>
 
-            <br /><br />
-
-        <!-- Button add Admin -->
-        <a href="#" class="btn-primary">Add Order</a>
-
-        <br /><br />
+            <br /><br /><br>
+        <?php 
+            if(isset($_SESSION['update'])){
+                echo $_SESSION['update'];
+                unset($_SESSION['update']);
+            }
+        ?>
+        <br><br>
 
         <table class="tbl-full">
             <tr>
                 <th>S.N</th>
-                <th>Full Name</th>
-                <th>Username</th>
+                <th>Food</th>
+                <th>price</th>
+                <th>Qty</th>
+                <th>Total</th>
+                <th>Order Date</th>
+                <th>statust</th>
+                <th>Customer Name</th>
+                <th>Contact</th>
+                <th>Email</th>
+                <th>Address</th>
                 <th>Actions</th>
             </tr>
 
-            <tr>
-                <td>1. </td>
-                <td>Gamaliel</td>
-                <td>Abiezer</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
+            <?php 
+                $sql = "SELECT * FROM tbl_order ORDER BY id DESC";
 
-            <tr>
-                <td>2. </td>
-                <td>Cindy</td>
-                <td>Kebo</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
+                $res = mysqli_query($conn, $sql);
 
-            <tr>
-                <td>3. </td>
-                <td>Eric</td>
-                <td>Tantrum</td>
-                <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                    <a href="#" class="btn-danger">Delete Admin</a>
-                </td>
-            </tr>
+                $count = mysqli_num_rows($res);
+
+                $sn = 1;
+
+                if($count>0){
+                    while($row=mysqli_fetch_assoc($res)){
+                        $id = $row['id'];
+                        $food = $row['food'];
+                        $price = $row['price'];
+                        $qty = $row['qty'];
+                        $total = $row['total'];
+                        $order_date = $row['order_date'];
+                        $status = $row['status'];
+                        $order_name = $row['customer_name'];
+                        $customer_contact = $row['customer_contact'];
+                        $customer_email = $row['customer_email'];
+                        $customer_address = $row['customer_address'];
+
+                        ?>
+                            <tr>
+                                <td><?php echo $sn++; ?></td>
+                                <td><?php echo $food; ?></td>
+                                <td><?php echo $price; ?></td>
+                                <td><?php echo $qty; ?></td>
+                                <td><?php echo $total; ?></td>
+                                <td><?php echo $order_date; ?></td>
+
+                                <td>
+                                    <?php 
+                                        if($status=="Ordered"){
+                                            echo "<label>$status</label>";
+                                        }else if($status=="On Delivery"){
+                                            echo "<label style='color: orange; >$status</label>";
+                                        }else if($status=="Delivered"){
+                                            echo "<label style='color: green; >$status</label>";
+                                        }else if($status=="Cancelled"){
+                                            echo "<label style='color: red; >$status</label>";
+                                        }
+                                    ?>
+                                </td>
+                                
+                                <td><?php echo $customer_name; ?></td>
+                                <td><?php echo $customer_contact; ?></td>
+                                <td><?php echo $customer_email; ?></td>
+                                <td><?php echo $customer_address; ?></td>
+                                
+                                <td>
+                                    <a href="<?php echo SITEURL; ?>admin/update-order.php?id=<?php echo $id; ?>" class="btn-secondary">Update Admin</a>
+                                </td>
+                            </tr>
+
+                        <?php
+                    }
+                }else{
+                    echo "<tr><td colspan='12' class='error'>Orders not Available</td></tr>";
+                }
+            ?>
+
         </table>
         </div>
     </div>
