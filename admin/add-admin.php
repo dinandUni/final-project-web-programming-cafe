66b77b9e3eb0a2d25c7cf6ca56694beb6cf08931
ChaserHades <ifofkgt@gmail.com>
2024-12-03 08:52:21 +0700

    Update add-admin.php
    Add Function Form

--- admin/add-admin.php
+++ admin/add-admin.php
@@ -3,6 +3,15 @@
     <div class="main-content">
         <div class="wrapper">
             <h1>Add Admin</h1>
+
+            <form action="" method="post">
+                <table class="tbl-30">
+                    <tr>
+                        <td>Full Name: </td>
+                        <td><input type="text" name="full_name" placeholder="Enter Your Name"></td>
+                    </tr>
+                </table>
+            </form>
         </div>
     </div>
     
